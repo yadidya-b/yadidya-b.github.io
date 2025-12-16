1b2b529e9835ea6bbdd0e4aeb21221fbe33f1acd
yadidya-b <67288184+yadidya-b@users.noreply.github.com>
2025-12-16 13:23:07 +0000

    Deploying to gh-pages from @ yadidya-b/yadidya-b.github.io@acce31aa65492add2f4b7fa1fe8db7e25b2e56c3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yadidya-b/academic-v2",
+    "data-repo": "yadidya-b/yadidya-b.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
